8ee2d9ea531217b1b6089965597740e97a821bbd
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 18:32:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   index = 100 
   min = []
   name_hash.each do |name, num|
-    if num < index
+    if num == min 
+      return nil 
+    else num < index
       min = name 
       index = num 
     end
