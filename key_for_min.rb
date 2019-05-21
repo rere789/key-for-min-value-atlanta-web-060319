389c4bb7d7e3ce4cddb2bab345b521ed6485e0d4
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 18:12:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  # min = []
-  # name_hash.collect do |i, num|
-  #   if num < min
+  min = []
+  name_hash.collect do |i, num|
+    if num < min
       
 
 end
